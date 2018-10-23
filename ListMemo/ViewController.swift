ebdcb68b7b7177c52b87538ea602940828b07571
hirotaka.s <hiro.0723boss.macpro@gmail.com>
2018-10-23 18:24:28 +0900

    Initial Commit

---
+++ ListMemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ListMemo
+//
+//  Created by 曽和寛貴 on 2018/10/23.
+//  Copyright © 2018 曽和寛貴. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ListMemoTests/ListMemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  ListMemoTests.swift
+//  ListMemoTests
+//
+//  Created by 曽和寛貴 on 2018/10/23.
+//  Copyright © 2018 曽和寛貴. All rights reserved.
+//
+
+import XCTest
+@testable import ListMemo
+
+class ListMemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
