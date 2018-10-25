15c9b593c33714981e54b6b30d3c7de97aef06f6
hirotaka.s <hiro.0723boss.macpro@gmail.com>
2018-10-25 20:20:03 +0900

    [add] Realmのモデルクラス

---
+++ ListMemo/List.swift
@@ -0,0 +1,30 @@
+//
+//  List.swift
+//  ListMemo
+//
+//  Created by 曽和寛貴 on 2018/10/25.
+//  Copyright © 2018 曽和寛貴. All rights reserved.
+//
+
+import RealmSwift
+
+class List: Object {
+    // 管理用 ID。プライマリーキー
+    @objc dynamic var id = 0
+    
+    // タイトル
+    @objc dynamic var title = ""
+    
+    // 内容
+    @objc dynamic var contents = ""
+    
+    /// 日時
+    @objc dynamic var date = Date()
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
