a4e9dd35b01e29276b2aea4af4a26cbd380d1459
hirotaka.s <hiro.0723boss.macpro@gmail.com>
2018-10-27 22:43:48 +0900

    [add] ローカル通知を設定

--- ListMemo/InputViewController.swift
+++ ListMemo/InputViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import UserNotifications
 
 class InputViewController: UIViewController {
 
@@ -47,7 +48,53 @@ class InputViewController: UIViewController {
             
         }
         
+        setNotification(list: list)
+        
         super.viewWillDisappear(animated)
+    }
+    
+//    タスクのローカル通知を登録する
+    func setNotification(list: List) {
+        
+        let content = UNMutableNotificationContent()
+//        タイトルと内容を設定(中身がない場合メッセージ無しで音だけの通知になるので「(xxなし)」を表示する)
+        if list.title == "" {
+            content.title = "(タイトルなし)"
+        } else {
+            content.title = list.title
+        }
+        
+        if list.contents == "" {
+            content.body = "(内容なし)"
+        } else {
+            content.body = list.contents
+        }
+        
+        content.sound = UNNotificationSound.default
+        
+//        ローカル通知が発動するtrigger（日付マッチ）を作成
+        let calender = Calendar.current
+        let dateConponents = calender.dateComponents([.year, .month, .day, .hour, .minute], from: list.date)
+        let trigger = UNCalendarNotificationTrigger.init(dateMatching: dateConponents, repeats: false)
+        
+//        identifier, content, triggerからローカル通知を作成（identifierが同じだとローカル通知を上書き保存）
+        let request = UNNotificationRequest.init(identifier: String(list.id), content: content, trigger: trigger)
+        
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+//            error が nil ならローカル通知の登録に成功したと表示します。errorが存在すればerrorを表示します。
+            print(error ?? "ローカル通知登録 OK")
+        }
+        
+//        未通知のローカル通知一覧をログ出力
+        center.getPendingNotificationRequests{ (requests: [UNNotificationRequest])  in for request in requests {
+            print("/---------------")
+            print(request)
+            print("---------------/")
+            }
+            
+        }
+        
     }
     /*
     // MARK: - Navigation
